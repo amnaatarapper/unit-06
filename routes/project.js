cbdc115cf00d2790983d202b0dd58414b0bc4824
Oussama <oussama.aitbaziz@gmail.com>
2019-07-20 13:37:58 +0100

    Main routes and views setup

---
+++ routes/project.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/project', (req, res) => {
+
+    res.render('project')
+  
+})
+
+
+module.exports = router;
